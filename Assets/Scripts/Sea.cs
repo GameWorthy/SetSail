347b7bbb9d54a89cc9ea4f2301f885d06d02e220
kaue-rosa <kaue.aftimus@gmail.com>
2015-09-15 23:15:27 -0700

    some art

---
+++ Assets/Scripts/Sea.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sea : MonoBehaviour {
+	private float speed = 15f;
+	private Vector2 offset = Vector2.zero;
+	public float Speed
+	{
+		get { return speed; }
+		set { speed = value; }
+	}
+	private Renderer rend = null;
+
+	void Start() {
+		rend = gameObject.GetComponent<Renderer>();
+	}
+
+	void Update() {
+		offset = new Vector2 (0,offset.y + speed * Time.deltaTime / 100);
+		rend.sharedMaterial.SetTextureOffset("_MainTex",offset);
+	}
+
+}
