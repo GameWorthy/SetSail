f3048c6c2b9f07d7a7db58a1adcbfce8667ebce6
kaue-rosa <kaue.aftimus@gmail.com>
2015-09-29 23:35:26 -0700

    naming levels

--- Assets/Scripts/DB/LevelMetadata.cs
+++ Assets/Scripts/DB/LevelMetadata.cs
@@ -8,13 +8,14 @@ public class LevelMetadata  {
 		//index 1: game_speed
 		//index 2: nautical_milestone
 		//index 3: sea_color
+		//index 4: level_name
 		{0, 3,   0, Color.white},
-		{1, 4,  50, new Color(133f/255f,255f/255f,205f/255f)},
-		{2, 6, 120, new Color(103f/255f,161f/255f,203f/255f)},
-		{3, 8, 260, new Color(103f/255f,203f/255f,132f/255f)},
-		{4, 10, 400, new Color(000f/255f,255f/255f,012f/255f)},
-		{5, 12, 800, new Color(255f/255f,164f/255f,000f/255f)},
-		{6, 14,  -1, new Color(205f/255f,249f/255f,253f/255f)},
+		{1, 4,  50, new Color(133f/255f,255f/255f,205f/255f),"Nilo River"},
+		{2, 6, 120, new Color(103f/255f,161f/255f,203f/255f),"Atlantic Ocean"},
+		{3, 8, 260, new Color(103f/255f,203f/255f,132f/255f),"Amazon River"},
+		{4, 10, 400, new Color(000f/255f,255f/255f,012f/255f),"Siberian Sea"},
+		{5, 12, 800, new Color(255f/255f,164f/255f,000f/255f),"Toxic Land"},
+		{6, 14,  -1, new Color(205f/255f,249f/255f,253f/255f),"Hell"},
 
 	};
 	
@@ -25,8 +26,12 @@ public class LevelMetadata  {
 	public static int GetLevelMilestone(int _level) {
 		return (int)levelsMetadata[_level,2];
 	}
-
+	
 	public static Color GetLevelSeaColor(int _level) {
 		return (Color)levelsMetadata [_level, 3];
+	}
+
+	public static string GetLevelName(int _level) {
+		return (string)levelsMetadata [_level, 4];
 	}
 }
